8d7b7c29e3e45ad4582adb531e830ee791f83348
adev07 <adityaanand1245@gmail.com>
2025-05-08 13:14:56 +0530

    cors fix for deployment

--- dist/app.js
+++ dist/app.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,16 +7,25 @@ exports.handler = void 0;
 // api/index.js
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const db_js_1 = __importDefault(require("./config/db.js"));
 const index_js_1 = __importDefault(require("./routes/index.js"));
 const error_js_1 = require("./middleware/error.js");
 const cors_1 = __importDefault(require("cors"));
 const serverless_http_1 = __importDefault(require("serverless-http"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-(() => __awaiter(void 0, void 0, void 0, function* () {
-    yield (0, db_js_1.default)();
-}))();
+// (async () => {
+//   await connectDB();
+// })();
+const allowedOrigins = [
+    "http://localhost:3000",
+    "https://msp-calculator-green.vercel.app",
+];
+app.use((0, cors_1.default)({
+    origin: allowedOrigins,
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.options("*", (0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
--- src/app.ts
+++ src/app.ts
@@ -10,17 +10,25 @@ import serverless from "serverless-http";
 dotenv.config();
 const app = express();
 
-(async () => {
-  await connectDB();
-})();
+// (async () => {
+//   await connectDB();
+// })();
+
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://msp-calculator-green.vercel.app",
+];
 
 app.use(
   cors({
-    origin: "*",
+    origin: allowedOrigins,
+    credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
 
 app.use(express.json());
 
